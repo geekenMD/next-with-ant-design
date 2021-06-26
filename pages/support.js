eb3c9a80539679031092f457a652a46809ec1366
geekenmd <justin@watsonnetworks.com>
2021-06-25 20:11:51 -0500

    init

--- pages/support.js
+++ pages/support.js
@@ -1,6 +1,6 @@
 import { Row, Col, Card, Typography, Button } from 'antd'
 import { GithubOutlined, createFromIconfontCN } from '@ant-design/icons';
-
+import Head from 'next/head'
 
 
 const IconFont = createFromIconfontCN({
@@ -9,7 +9,18 @@ const IconFont = createFromIconfontCN({
 
 export default function () {
 
-  return <Row>
+  return <div>
+    <Head>
+  <title>Geeken Test</title>
+  <meta name="twitter:card" content="summary" />
+<meta name="twitter:site" content="@WatchBrime" />
+<meta name="twitter:creator" content="@geeken" />
+<meta property="og:url" content="http://bits.blogs.nytimes.com/2011/12/08/a-twitter-for-my-sister/" />
+<meta property="og:title" content="A Twitter for My Sister" />
+<meta property="og:description" content="In the early days, Twitter grew so quickly that it was almost impossible to add new features because engineers spent their time trying to keep the rocket ship from stalling." />
+<meta property="og:image" content="http://graphics8.nytimes.com/images/2011/12/08/technology/bits-newtwitter/bits-newtwitter-tmagArticle.jpg" />
+</Head>
+  <Row>
     <Col span={24}>
       <Card>
 
@@ -74,4 +85,5 @@ export default function () {
       </Card>
     </Col>
   </Row>
+  </div>
 }
